af8b395a7e52e8d5ae2a20a93d5508370d353d6e
Rogelio Ventura <rogeliovr94@gmail.com>
2018-04-18 14:48:33 -0500

    env changes

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	$dbHost = "localhost";
 	$dbName = "electronica";
 	$dbUser = "root";
-	$dbPassword = "prst3977";
+	$dbPassword = "Ro9311o/";
 	try{
 		$pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
