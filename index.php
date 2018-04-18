216db3c71f53577d7055ae63ee6653fbd9019ac8
Rogelio Ventura <rogeliovr94@gmail.com>
2018-04-18 15:10:21 -0500

    display data

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Proyecto Electrónica</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-	<?php require_once 'getData.php'; ?>
+	<?php require_once 'config.php'; ?>
 </head>
 
 <body>
